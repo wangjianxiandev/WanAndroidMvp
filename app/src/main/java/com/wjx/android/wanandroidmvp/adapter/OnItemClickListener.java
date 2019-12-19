442c3331da8026af664a627cbdf8cfe87e6eead0
wangjianxiandev <jianxianwang198@163.com>
2019-12-19 22:04:11 +0800

    搭建文章详情，并进行布局优化

---
+++ app/src/main/java/com/wjx/android/wanandroidmvp/adapter/OnItemClickListener.java
@@ -0,0 +1,12 @@
+package com.wjx.android.wanandroidmvp.adapter;
+
+/**
+ * Created with Android Studio.
+ * Description:
+ *
+ * @author: 王拣贤
+ * @date: 2019/12/19
+ * Time: 21:17
+ */
+public interface OnItemClickListener {
+}
---
+++ app/src/main/java/com/wjx/android/wanandroidmvp/base/utils/JumpWebUtils.java
@@ -0,0 +1,12 @@
+package com.wjx.android.wanandroidmvp.base.utils;
+
+/**
+ * Created with Android Studio.
+ * Description:
+ *
+ * @author: 王拣贤
+ * @date: 2019/12/19
+ * Time: 21:32
+ */
+public class JumpWebUtils {
+}
